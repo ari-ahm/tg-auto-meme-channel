52bb3017a55ff623423326d922169f9efe56419f
Mohammad Arian Ahmidi <arian.ahmidi@gmail.com>
2024-03-04 01:52:01 +0330

    refactor: moved jokes hashtag to the api list

--- posts/joke.py
+++ posts/joke.py
@@ -2,11 +2,11 @@ import requests
 import random
 
 joke_apis = [
-    lambda : requests.get("https://api.chucknorris.io/jokes/random").json()["value"]
+    lambda : requests.get("https://api.chucknorris.io/jokes/random").json()["value"] + "\n\n#ChuckNorris"
 ]
 
 def getJoke() :
     return random.choice(joke_apis)()
 
 def post(bot, env) :
-    bot.send_message(env["secret"]["tg-channel-numeric-id"], "{}\n\n#ChuckNorris\n{}".format(getJoke(), env["secret"]["tg-channel-id"]))
+    bot.send_message(env["secret"]["tg-channel-numeric-id"], "{}\n{}".format(getJoke(), env["secret"]["tg-channel-id"]))
